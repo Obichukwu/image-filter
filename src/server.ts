e667faecd1ef958ab40a91422441ba5fff9b6c6a
Obichukwu O <obichukwu.zoni@gmail.com>
2022-08-28 02:14:26 +0100

    Complete Todo Task

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import express, { Request, Response } from 'express';
+
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
 
 (async () => {
 
@@ -9,38 +9,61 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
-  app.use(bodyParser.json());
+  app.use(express.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
-  
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get("/", async (req, res) => {
     res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  });
+
+  const isValidUrl = (urlString: string) => {
+    try {
+      return Boolean(new URL(urlString));
+    }
+    catch (e) {
+      return false;
+    }
+  }
+
+  app.get("/filteredimage", async (req: Request, res: Response) => {
+    let { image_url } = req.query;
+
+    // checks if image_url is provided in querystring
+    if (!image_url) {
+      return res.status(400)
+        .send(`image_url is required`);
+    }
+
+    // checks if the supplied image_url is a valid url
+    if (!isValidUrl(image_url)) {
+      return res.status(400)
+        .send(`image_url is not a valid url. ensure you add the protocol part of the url. eg https://`);
+    }
+
+    let filteredImage: string;
+    try {
+      // process the image
+      filteredImage = await filterImageFromURL(image_url);
+    } catch (error) {
+      return res.status(500)
+        .send(`an error occurred when processing url, check the image_url`);
+    }
+
+
+    // send the filtered image to the client
+    // sendFile is async, therefore, delete the image with a callback
+    // to avoid cluttering the fs
+    res.sendFile(filteredImage, (err) => {
+      deleteLocalFiles([filteredImage]);
+    })
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
